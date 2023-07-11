c390e845040bfe896fb7ad54a86cc276b3941ea7
TimFromKrim <timyr0615@gmail.com>
2023-07-11 15:02:03 +0400

    small chenges for mobile2

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@ const app = new App({
 });
 
 export default App;
+
+function preventDefault(e) {
+	e.preventDefault();
+}
+
+function disableScroll() {
+	document.body.addEventListener("touchmove", preventDefault, {
+		passive: false,
+	});
+}
+function enableScroll() {
+	document.body.removeEventListener("touchmove", preventDefault);
+}
+
+disableScroll();
